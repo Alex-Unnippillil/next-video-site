28fd45c877804ddb6a28fe1505326659c10950ef
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-19 19:26:05 -0400

    Add cookie consent and policy pages with analytics retention

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Next Video Site</h1>
+      <p>Welcome to our site with analytics consent.</p>
+    </main>
+  );
+}
---
+++ pages/terms.js
@@ -0,0 +1,11 @@
+export default function Terms() {
+  return (
+    <main>
+      <h1>Terms and Conditions</h1>
+      <p>
+        By using this site you agree to our standard terms and the use of
+        analytics data as described in our Privacy Policy.
+      </p>
+    </main>
+  );
+}

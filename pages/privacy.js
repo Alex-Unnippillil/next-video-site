e332617e7c9487fd750ef8147e4cdf7ddd783ab2
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-19 21:29:36 -0400

    Fix conflicts: removed problematic lines from 13 files

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,11 +1,1 @@
-export default function Privacy() {
-  return (
-    <main>
-      <h1>Privacy Policy</h1>
-      <p>
-        This site uses cookies for analytics after you provide consent. Data is
-        retained for a limited time to respect your privacy.
-      </p>
-    </main>
-  );
-}
+﻿

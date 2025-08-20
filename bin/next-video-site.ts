b7294f41f0938c1de408de2a895a099dfec962f9
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-19 21:33:07 -0400

    Fix conflicts: removed problematic lines from 8 files

--- bin/next-video-site.ts
+++ bin/next-video-site.ts
@@ -1,7 +1,1 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import { App } from 'aws-cdk-lib';
-import { AuroraPgStack } from '../lib/aurora-pg-stack';
-
-const app = new App();
-new AuroraPgStack(app, 'AuroraPgStack');
+﻿

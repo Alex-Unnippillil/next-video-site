eca8688980109aec7906102e43334129d26fb3e6
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-20 01:52:28 -0700

    Add sample video recommendations API

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -1,1 +1,22 @@
-﻿// File cleared by force push to main
+export default function handler(req, res) {
+  res.status(200).json([
+    {
+      id: 1,
+      title: 'Understanding Async/Await',
+      thumbnail: '/thumbnails/async-await.png',
+      url: 'https://example.com/videos/1'
+    },
+    {
+      id: 2,
+      title: 'JavaScript Promises in Depth',
+      thumbnail: '/thumbnails/promises.png',
+      url: 'https://example.com/videos/2'
+    },
+    {
+      id: 3,
+      title: 'React Hooks Crash Course',
+      thumbnail: '/thumbnails/hooks.png',
+      url: 'https://example.com/videos/3'
+    }
+  ]);
+}

6537201962ce75dbd9b44c0abb2e1788f06541d8
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-19 21:28:57 -0400

    Fix conflicts: removed problematic lines from 7 files

--- src/ecs/app.js
+++ src/ecs/app.js
@@ -1,16 +1,1 @@
-const express = require('express');
-const { AWSXRay } = require('../tracing');
-
-const app = express();
-app.use(AWSXRay.express.openSegment('ecs'));
-
-app.get('/', (req, res) => {
-  res.send('Hello from ECS');
-});
-
-app.use(AWSXRay.express.closeSegment());
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`ECS service listening on ${port}`);
-});
+﻿
--- src/lambda/handler.js
+++ src/lambda/handler.js
@@ -1,10 +1,1 @@
-const { AWSXRay } = require('../tracing');
-
-const baseHandler = async (event) => {
-  return {
-    statusCode: 200,
-    body: 'Hello from Lambda'
-  };
-};
-
-exports.handler = AWSXRay.captureLambdaHandler(baseHandler);
+﻿
